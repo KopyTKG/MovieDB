c31f9665881f8caf6a063d5455314fa7addd567b
Martin Kopecky <kopecky.martin259@gmail.com>
2023-10-01 18:50:55 +0200

    UI update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-
 import Box from "@/modules/box";
+import API from "@/modules/controllers/api.controller";
 
-export default function Home() {
-
+export default async function Home() {
+  let movieFetcher = new API(`${process.env.BASE_URL}/api/getRandomPoster`)
+  let rawData = await movieFetcher.getData(0)
+  let data = rawData.message
 
   return (
     <main className="Page-home">
-      <a href="/movies"> <Box>Movies</Box></a>
-      <a href="/series"> <Box>Series</Box></a>
+      <a href="/movies" id="movies"> <Box poster={data.src}>Movies</Box></a>
+      <a href="/series" id="series"> <Box>Series</Box></a>
     </main>
   )
 }
--- src/modules/box.tsx
+++ src/modules/box.tsx
@@ -1,7 +1,7 @@
 export default function Box(props: any) {
     return(
         <>
-            <div className="box">
+            <div className="box" style={{backgroundImage: "url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"+props.poster+")"}}>
                 <div className="backsplash">
                     {props.children}
                 </div>
