d304afbb8df7ccddb217ff1e5ba68e249c09c68c
“Martin <BulikopM@gmail.com>
2023-10-02 13:36:26 +0200

    main page failsafe added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import API from "@/modules/controllers/api.controller";
 export default async function Home() {
   let movieFetcher = new API(`${process.env.BASE_URL}/api/rngPoster`)
   let rawData = await movieFetcher.getData(0)
-  let data = rawData.message
+  let data = rawData.message? rawData.message : { src: ""}
   return (
     <main className="Page-home">
       <a href="/movies" id="movies"> <Box poster={data.src}>Movies</Box></a>
