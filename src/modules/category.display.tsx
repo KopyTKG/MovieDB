f1a8620f70fd0f93966ea5f4f4694a56c0bf4b9e
Martin Kopecký <kopecky@thekrew.app>
2023-11-15 15:21:12 +0100

    V3.6.2 #56

--- src/modules/category.display.tsx
+++ src/modules/category.display.tsx
@@ -7,6 +7,29 @@ import ErrorPage from "./error.page";
 import { Card, CardBody, Image } from "@nextui-org/react";
 import Link from "next/link";
 
+const tmp = [
+"bg-red-300/70",
+"bg-red-400/70",
+"bg-red-600/70",
+"bg-orange-300/70",
+"bg-orange-400/70",
+"bg-orange-600/70",
+"bg-amber-300/70",
+"bg-amber-400/70",
+"bg-amber-600/70",
+"bg-lime-300/70",
+"bg-lime-400/70",
+"bg-lime-600/70",
+"bg-teal-300/70",
+"bg-teal-400/70",
+"bg-teal-600/70",
+"bg-purple-300/70",
+"bg-purple-400/70",
+"bg-purple-600/70",
+"bg-rose-300/70",
+"bg-rose-400/70",
+"bg-rose-600/70"]
+
 export default function Category() {
   const [genres, setGenres] = useState([]);
   const [error, setError] = useState<any>(null);
@@ -43,7 +66,7 @@ export default function Category() {
   }
   return (
     <>
-      <div className="w-full grid grid-cols-1 justify-items-center sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  gap-10">
+      <div className="w-full grid grid-cols-1 justify-items-center sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  gap-10 mt-5 mb-20">
         {genres.map((genre: any) => (
           <Link
             href={`/collection?g=${genre.name}`}
@@ -51,20 +74,20 @@ export default function Category() {
             className="w-max h-max relative flex self-center"
           > 
           <div
-            className="w-full h-full absolute rounded-2xl overflow-hidden z-1"
+            className="w-full h-full absolute rounded-2xl overflow-hidden z-1 flex items-center justify-center"
           >
 
             <Image
               alt="Movie poster"
               src={
                 `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/` +
-                'bI7lGR5HuYlENlp11brKUAaPHuO.jpg'
+                genre.backdrop
               }
               />
             </div>
-            <Card className="max-w-full w-[20rem] bg-green-500/80 z-10">
+            <Card className={`max-w-full w-[20rem] ${genre.color}/70 z-10`}>
               <CardBody className="flex items-center py-20">
-                <span className="text-2xl font-bold">{genre.name}</span>
+                <span className="text-2xl font-bold text-black">{genre.name}</span>
               </CardBody>
             </Card>
           </Link>
