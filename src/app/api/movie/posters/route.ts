da544137c26f3f5a1c8bd64f47f1a26a671109ac
Martin Kopecký <kopecky@thekrew.app>
2023-11-21 16:30:05 +0100

    Media added #80

---
+++ src/app/api/movie/posters/route.ts
@@ -0,0 +1,47 @@
+import redis from '@/lib/redis'
+import prisma from '../../../../../prisma/client'
+import JWT from '@/modules/controllers/jwt.controller'
+import * as jose from 'jose'
+
+export const dynamic = 'force-dynamic'
+
+export async function POST(req: Request) {
+ try {
+  const headers = req.headers
+  const token = String(headers.get('authorization')?.split(' ')[1])
+  const settings = await req.json()
+
+  // JWT stuff
+  const jwt = new JWT()
+  const secret = await jwt.getPublic()
+
+  try {
+   const { payload } = await jose.jwtVerify(token, secret, {
+    issuer: 'urn:thekrew:issuer',
+    audience: 'urn:thekrew:audience',
+   })
+   if (payload) {
+    const cache = await redis.get(settings + 'posters')
+    if (cache) {
+     return Response.json(JSON.parse(cache))
+    } else {
+     let raw = await prisma.posters.findUnique({
+      where: {
+       id: parseInt(settings),
+      },
+     })
+     if (raw) {
+      await redis.set(settings + 'posters', JSON.stringify(raw), 'EX', 60 * 60 * 2)
+      return Response.json(raw)
+     } else {
+      return Response.json(raw)
+     }
+    }
+   }
+  } catch (e) {
+   return Response.json('Invalid token')
+  }
+ } catch (e) {
+  return Response.json(e)
+ }
+}
