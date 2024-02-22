ceb829489d25ddae6156aff3defe173461b40f6e
Martin Kopecký <kopecky@thekrew.app>
2024-02-22 10:52:11 +0100

    still 3.9.1

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,10 +1,13 @@
 import Movies from '@/modules/display.movies'
 import SearchNavbar from '@/modules/search.navbar'
+import { Suspense } from 'react'
 
 export default function Page() {
  return (
   <>
-   <SearchNavbar />
+   <Suspense>
+    <SearchNavbar />
+   </Suspense>
    <div className="min-h-screen w-full flex flex-col items-center">
     <Movies />
    </div>
