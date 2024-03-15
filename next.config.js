71c1cdf5d9c54d1a1c8700a4587e2c45c1acc87d
Martin Kopecký <kopecky@thekrew.app>
2024-03-15 17:36:07 +0100

    suspense is f-ed up

--- next.config.js
+++ next.config.js
@@ -1,2 +1,5 @@
-/** @type {import('next').NextConfig} */
-module.exports = {}
+module.exports = {
+ experimental: {
+  missingSuspenseWithCSRBailout: false,
+ },
+}
--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,13 +1,10 @@
 import Movies from '@/modules/display.movies'
 import SearchNavbar from '@/modules/search.navbar'
-import { Suspense } from 'react'
 
 export default function Page() {
  return (
   <>
-   <Suspense>
-    <SearchNavbar />
-   </Suspense>
+   <SearchNavbar />
    <div className="min-h-screen w-full flex flex-col items-center">
     <Movies />
    </div>
